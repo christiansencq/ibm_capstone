d445fd40c0267be0948a5d85e9d43828b908641c
Charles Q Christiansen <59367334+christiansencq@users.noreply.github.com>
2021-11-18 12:39:09 -0500

    Update main.py

--- functions/sample/python/main.py
+++ functions/sample/python/main.py
@@ -14,19 +14,11 @@ import requests
 
 def main(dict):
 
-
-
-        # client=Cloudant(
-        #     dict["COUCH_USERNAME"],
-        #     dict["ZHDaKveTQOV3dbrrx2lx17uGCXXFbw8kAU0CA5hEZ7IT"],
-        #     url=dict["https://apikey-v2-ex50zt74fdon5ojl817z8oh0t9p2ksi4j7shgcpqhb0:88d46706d5b61082f03e097985284b9c@b8313b34-4a62-4bbc-912e-c754459259ca-bluemix.cloudantnosqldb.appdomain.cloud"],
-        #     connect=True,
-        # )
     databaseName = "reviews"
     try:
-        account_user_name = "apikey-v2-ex50zt74fdon5ojl817z8oh0t9p2ksi4j7shgcpqhb0"
-        apikey = "ZHDaKveTQOV3dbrrx2lx17uGCXXFbw8kAU0CA5hEZ7IT"
-        url = "https://apikey-v2-ex50zt74fdon5ojl817z8oh0t9p2ksi4j7shgcpqhb0:88d46706d5b61082f03e097985284b9c@b8313b34-4a62-4bbc-912e-c754459259ca-bluemix.cloudantnosqldb.appdomain.cloud"
+        account_user_name = ""
+        apikey = ""
+        url = ""
         client = Cloudant.iam(
             account_name=account_user_name,
             api_key=apikey,
